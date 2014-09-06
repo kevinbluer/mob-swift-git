0f1872bff90cc3c521101c43ce0c11c672ad4e89
Kevin Bluer <kevin@bluer.com>
2014-09-06 20:57:54 +0800

    Adding Hello World

---
+++ TeamHelloWorld.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+println("Hello from AA")
+println("Hello from BL")
+println("Hello from CW")
+println("Hello from KK")
+println("Hello from KB")
+println("Hello from LC")
+println("Hello from VL")
+println("Hello from WK")
